cf3dd15e3fc571da8be1403711f5195eef421cb1
Cody Speroff <cnsperoff@gmail.com>
2017-11-20 22:24:41 -0600

    updated config/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
-    scope "/:locale" do
-        resources :books
-    end
 
     # defailt locale when one is not specified: localhost:3001/books
     scope "(:locale)", locale: /en|jp/ do
-        resources :books
+        resources :works
+        resources :artists
+        resources :components
     end
 
-    resources :components
-    resources :artists
-    resources :works
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
