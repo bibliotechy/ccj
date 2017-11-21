b7668b5b170af27d65fdd02fc189c5ccec948b1b
Chad Nelson <chadbnelson@gmail.com>
2017-11-21 07:46:35 -0500

    Add Blacklight and run generator

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
+gem 'blacklight'
 
 
 
@@ -54,3 +55,16 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+group :development, :test do
+  gem 'solr_wrapper', '>= 0.3'
+end
+
+gem 'rsolr', '>= 1.0'
+
+group :development, :test do
+end
+
+gem 'jquery-rails'
+gem 'devise'
+gem 'devise-guests', '~> 0.6'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,29 @@
 Rails.application.routes.draw do
 
+  
+    concern :searchable, Blacklight::Routes::Searchable.new
+
+  resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
+    concerns :searchable
+  end
+
+  devise_for :users
+  concern :exportable, Blacklight::Routes::Exportable.new
+
+  resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
+    concerns :exportable
+  end
+
+  resources :bookmarks do
+    concerns :exportable
+
+    collection do
+      delete 'clear'
+    end
+  end
+
+  mount Blacklight::Engine => '/'
+  root to: "catalog#index"
     # defailt locale when one is not specified: localhost:3001/books
     scope "(:locale)", locale: /en|jp/ do
         resources :works
